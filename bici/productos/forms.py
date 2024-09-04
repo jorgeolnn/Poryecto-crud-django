7993907414c51295961d4ddcf434b9c6f147ede9
jorgeolnn <jor.lagunas@duocuc.cl>
2024-09-04 19:08:09 -0400

    Se agregó una validación de extensión de imágen

--- bici/productos/forms.py
+++ bici/productos/forms.py
@@ -4,7 +4,9 @@ from .models import Profile, Producto, Contacto
 from django import forms
 from .validators import MaxSizeFileValidator
 from django.forms import ValidationError
+from django.core.validators import FileExtensionValidator
 
+# from PIL import Image
 # Registro de usuarios, perfil, login.
 class CustomUserCreationForm(UserCreationForm): 
     
@@ -33,16 +35,31 @@ class ProfileUpdateForm(forms.ModelForm):
 
 ################################################
 
-class ProductoForm(forms.ModelForm):
+# def validar_extension_imagen(field_file):
+#     if field_file:
+#         extensión = field_file.name.split('.')[-1].lower()
+#         if extensión not in ['jpg', 'png', 'jpeg']:
+#             raise ValidationError('Formato de imagen no permitido')
 
+
+    
+# def validar_dimensiones_imagen(field_file):
+#     imagen = Image.open(field_file)
+#     ancho, alto = imagen.size
+    
+#     # Validar dimensiones
+#     if ancho < 800 or alto < 600:
+#         raise ValidationError('La imagen debe tener un ancho mínimo de 800px y un alto mínimo de 600px')
+
+
+class ProductoForm(forms.ModelForm):
     nombre = forms.CharField(min_length=3, max_length=50)
-    imagen = forms.ImageField(required=False, validators=[MaxSizeFileValidator(max_file_size=2)]) # Campo no obligatorio
+    imagen = forms.ImageField(validators=[MaxSizeFileValidator(max_file_size=2), FileExtensionValidator(allowed_extensions=['jpg', 'png', 'jpeg'], message="Solo se permiten archivos con extensiones .jpg, .png o .jpeg")])
     precio = forms.IntegerField(min_value=1, max_value=1500000)
 
     def clean_nombre(self):
         nombre = self.cleaned_data["nombre"]
         existe = Producto.objects.filter(nombre__iexact=nombre).exists()
-
         if existe:
             raise ValidationError("Este nombre ya existe")
         return nombre
@@ -50,10 +67,10 @@ class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ['nombre', 'precio', 'descripcion', 'nuevo', 'marca', 'fecha_fabricacion', 'imagen']
-
         widgets = {
             "fecha_fabricacion": forms.SelectDateWidget()
         }
+
 
 ##
 class ContactoForm(forms.ModelForm):
